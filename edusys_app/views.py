7ac8f1afd5cb0c53611092930045e14d1ea5c80d
Eugene Mefyod <96493615+heavenyoung1@users.noreply.github.com>
2022-09-17 17:00:26 +0300

    Добавлены: 1. Страница home 2. Страница signup (регистрация новых пользователей) 3. Базовый макет для всех страниц 4. Добавлено приложение edusys_app

--- edusys_app/views.py
+++ edusys_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'edusys_app/home.html')
+
+def signup(request):
+    return render(request, 'edusys_app/signup.html', {'form':UserCreationForm()})
