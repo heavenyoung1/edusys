9d8f157f6de700a33c467ebaf89ae936f5afc332
Eugene Mefyod <96493615+heavenyoung1@users.noreply.github.com>
2022-09-19 11:19:10 +0300

    мелкие исправления, сделана новая форма для регистрации пользователей

--- edusys_app/models.py
+++ edusys_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class University(models.Model):
+    id = models.DecimalField()
+    full_name_university = models.CharField(max_length=200)
+    short_name_university = models.CharField(max_length=200)
+
--- edusys_app/views.py
+++ edusys_app/views.py
@@ -3,13 +3,16 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
+from edusys_app.forms import RegistrationForm
 
 # Create your views here.
 
 def home(request):
+    """Вызов домашней страницы"""
     return render(request, 'edusys_app/home.html')
 
 def signupuser(request):
+    """Регистрация пользователей"""
     if request.method == 'GET':
         return render(request, 'edusys_app/signupuser.html', {'form':UserCreationForm()})
     else:
@@ -23,12 +26,29 @@ def signupuser(request):
         else:
             return render(request, 'edusys_app/signupuser.html', {'form':UserCreationForm(), 'error':'Пароли не совпадают'})
 
+def register(request):
+    """Кастомная форма регистрации пользователей"""
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/home')
+
+    #Доделать исключения
+    else:
+        # form = RegistrationForm()
+        # args = {'form': form}
+        # return render(request, )
+        pass
+
 def logoutuser(request):
+    """Выход пользователя"""
     if request.method == 'POST':
         logout(request)
         return redirect('home')
 
 def loginuser(request):
+    """Вход пользователя в учётную запись"""
     if request.method == 'GET':
         return render(request, 'edusys_app/loginuser.html', {'form':AuthenticationForm()})
     else:
